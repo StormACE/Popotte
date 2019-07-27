db58c790b646e3d6d10a168b46856026e4152189
Martin Laflamme <kryptikcreations@gmail.com>
2019-07-27 17:28:44 -0400

    Fix major bug
    Fix bug with the save reminder at first installation

--- RichTextEditor/My Project/AssemblyInfo.vb
+++ RichTextEditor/My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("5.2.1.70")>
-<Assembly: AssemblyFileVersion("5.2.1.70")>
+<Assembly: AssemblyVersion("5.2.2.71")>
+<Assembly: AssemblyFileVersion("5.2.2.71")>
 
 <Assembly: NeutralResourcesLanguageAttribute("fr-CA")> 
