7b20496561e8613cf159d8996615f97c186d0adb
Martin Laflamme <34052775+StormACE@users.noreply.github.com>
2021-06-08 11:42:52 -0400

    little fix

--- RichTextEditor/dlgStartup.Designer.vb
+++ RichTextEditor/dlgStartup.Designer.vb
@@ -43,20 +43,18 @@ Partial Class dlgStartup
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Palatino Linotype", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.SlateBlue
-        Me.Label1.Location = New System.Drawing.Point(397, 36)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Location = New System.Drawing.Point(596, 55)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(344, 87)
+        Me.Label1.Size = New System.Drawing.Size(513, 129)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Popotte v5"
         '
         'Label2
         '
         Me.Label2.Font = New System.Drawing.Font("Segoe UI", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(394, 133)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Location = New System.Drawing.Point(591, 205)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(339, 55)
+        Me.Label2.Size = New System.Drawing.Size(508, 85)
         Me.Label2.TabIndex = 9
         Me.Label2.Text = "Veuillez choisir le dossier où seront sauvegardées vos recettes."
         Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
@@ -66,10 +64,9 @@ Partial Class dlgStartup
         Me.RadioButton1.AutoSize = True
         Me.RadioButton1.BackgroundImage = CType(resources.GetObject("RadioButton1.BackgroundImage"), System.Drawing.Image)
         Me.RadioButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.RadioButton1.Location = New System.Drawing.Point(446, 246)
-        Me.RadioButton1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.RadioButton1.Location = New System.Drawing.Point(669, 378)
         Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(14, 13)
+        Me.RadioButton1.Size = New System.Drawing.Size(21, 20)
         Me.RadioButton1.TabIndex = 10
         Me.RadioButton1.TabStop = True
         Me.RadioButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
@@ -79,10 +76,9 @@ Partial Class dlgStartup
         '
         Me.PictureBox2.BackgroundImage = CType(resources.GetObject("PictureBox2.BackgroundImage"), System.Drawing.Image)
         Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.PictureBox2.Location = New System.Drawing.Point(471, 213)
-        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.PictureBox2.Location = New System.Drawing.Point(706, 328)
         Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(175, 72)
+        Me.PictureBox2.Size = New System.Drawing.Size(262, 111)
         Me.PictureBox2.TabIndex = 11
         Me.PictureBox2.TabStop = False
         '
@@ -90,10 +86,9 @@ Partial Class dlgStartup
         '
         Me.PictureBox3.BackgroundImage = CType(resources.GetObject("PictureBox3.BackgroundImage"), System.Drawing.Image)
         Me.PictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.PictureBox3.Location = New System.Drawing.Point(470, 289)
-        Me.PictureBox3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.PictureBox3.Location = New System.Drawing.Point(705, 445)
         Me.PictureBox3.Name = "PictureBox3"
-        Me.PictureBox3.Size = New System.Drawing.Size(175, 51)
+        Me.PictureBox3.Size = New System.Drawing.Size(262, 78)
         Me.PictureBox3.TabIndex = 12
         Me.PictureBox3.TabStop = False
         '
@@ -101,10 +96,9 @@ Partial Class dlgStartup
         '
         Me.RadioButton4.AutoSize = True
         Me.RadioButton4.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RadioButton4.Location = New System.Drawing.Point(446, 362)
-        Me.RadioButton4.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.RadioButton4.Location = New System.Drawing.Point(669, 557)
         Me.RadioButton4.Name = "RadioButton4"
-        Me.RadioButton4.Size = New System.Drawing.Size(241, 25)
+        Me.RadioButton4.Size = New System.Drawing.Size(363, 36)
         Me.RadioButton4.TabIndex = 13
         Me.RadioButton4.TabStop = True
         Me.RadioButton4.Text = "   Dossier Local Personnalisé"
@@ -113,22 +107,20 @@ Partial Class dlgStartup
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(8, 459)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Location = New System.Drawing.Point(12, 706)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(152, 13)
+        Me.Label3.Size = New System.Drawing.Size(222, 20)
         Me.Label3.TabIndex = 14
-        Me.Label3.Text = "© Martin Laflamme 2003/2018"
+        Me.Label3.Text = "© Martin Laflamme 2003/2021" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         '
         'Button1
         '
         Me.Button1.Cursor = System.Windows.Forms.Cursors.Arrow
         Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.System
         Me.Button1.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(645, 451)
-        Me.Button1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Button1.Location = New System.Drawing.Point(968, 694)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(94, 25)
+        Me.Button1.Size = New System.Drawing.Size(141, 38)
         Me.Button1.TabIndex = 15
         Me.Button1.Text = "Suivant"
         Me.Button1.UseVisualStyleBackColor = True
@@ -136,10 +128,9 @@ Partial Class dlgStartup
         'RadioButton2
         '
         Me.RadioButton2.AutoSize = True
-        Me.RadioButton2.Location = New System.Drawing.Point(446, 309)
-        Me.RadioButton2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.RadioButton2.Location = New System.Drawing.Point(669, 475)
         Me.RadioButton2.Name = "RadioButton2"
-        Me.RadioButton2.Size = New System.Drawing.Size(14, 13)
+        Me.RadioButton2.Size = New System.Drawing.Size(21, 20)
         Me.RadioButton2.TabIndex = 16
         Me.RadioButton2.TabStop = True
         Me.RadioButton2.UseVisualStyleBackColor = True
@@ -148,18 +139,17 @@ Partial Class dlgStartup
         '
         Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
         Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.PictureBox1.Location = New System.Drawing.Point(29, 36)
-        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.PictureBox1.Location = New System.Drawing.Point(44, 55)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(345, 350)
+        Me.PictureBox1.Size = New System.Drawing.Size(518, 538)
         Me.PictureBox1.TabIndex = 1
         Me.PictureBox1.TabStop = False
         '
         'dlgStartup
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(751, 504)
+        Me.ClientSize = New System.Drawing.Size(1126, 775)
         Me.Controls.Add(Me.RadioButton2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label3)
@@ -172,6 +162,7 @@ Partial Class dlgStartup
         Me.Controls.Add(Me.PictureBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "dlgStartup"
