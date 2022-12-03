3458612fb1ee774f7292026769eeb3366a1a272c
Martin Laflamme <34052775+StormACE@users.noreply.github.com>
2022-12-03 10:34:37 -0500

    Fix read only controls
    some controls was not disabled

--- RichTextEditor/My Project/AssemblyInfo.vb
+++ RichTextEditor/My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("5.4.0.101")>
-<Assembly: AssemblyFileVersion("5.4.0.101")>
+<Assembly: AssemblyVersion("5.4.0.102")>
+<Assembly: AssemblyFileVersion("5.4.0.102")>
 
 <Assembly: NeutralResourcesLanguageAttribute("fr-CA")> 
